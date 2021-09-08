53377f2057c099972231b43c1a4de1c4ecf46210
Faruque-10 <faruque_10@yahoo.com>
2021-09-08 17:53:41 +0100

    My first commit

--- src/main/java/CurrysRegistration.java
+++ src/main/java/CurrysRegistration.java
@@ -15,13 +15,22 @@ public class CurrysRegistration {
         // don't touch the code above this line.
         // Locators
         By acceptCookies =  By.xpath("//*[@id=\"onetrust-accept-btn-handler\"]");
-        By Signin = By.xpath("//*[@id=\"header\"]/div/div/div/div[3]/a[4]/span[2]");
+        By signin = By.xpath("//*[@id=\"header\"]/div/div/div/div[3]/a[4]/span[2]");
+        By emailAddress = By.xpath("//*[@id=\"input-sEmail\"]");
+        //By iDontHaveAnAccount = By.xpath("//*[@id=\"content\"]/div[2]/div/div/form/div[1]/div[1]/label/div");
+        By iAlreadyHaveAnAccount = By.xpath("//*[@id=\"content\"]/div[2]/div/div/form/div[1]/div[2]/label/div");
+        By password = By.xpath("//*[@id=\"input-sPassword\"]");
+        By SignIn = By.xpath("//*[@id=\"content\"]/div[2]/div/div/form/div[3]/button");
         //By firstNameField =  By.xpath("//*[@id=\"js-cookie-modal-level-one\"]/div/main/div/button[2]");
         try {
             // launch chrome and go to url
             driver.get("https://www.currys.ie");
             driver.findElement(acceptCookies).click();
-            driver.findElement(Signin).click();
+            driver.findElement(signin).click();
+            driver.findElement(emailAddress).sendKeys("faruque_10@yahoo.com");
+            driver.findElement(iAlreadyHaveAnAccount).click();
+            driver.findElement(password).sendKeys("MD@_obaidur78");
+            driver.findElement(SignIn).click();
             //driver.findElement(firstNameField).sendKeys("First name");
             // Don't touch the code below this line
             System.out.println("Your Test Passed");
